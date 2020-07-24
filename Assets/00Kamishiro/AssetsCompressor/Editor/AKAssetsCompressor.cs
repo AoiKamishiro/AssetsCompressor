d71a0a399f22b686538a41f138d33334edb95698
AoiKamishiro <syouho.mizumoto@gmail.com>
2020-07-24 22:06:55 +0900

    Add link button to Github

--- Assets/00Kamishiro/AssetsCompressor/Editor/AKAssetsCompressor.cs
+++ Assets/00Kamishiro/AssetsCompressor/Editor/AKAssetsCompressor.cs
@@ -1,24 +1,26 @@
 ﻿/*
- Copyright (c) 2020 AoiKamishiro/神城葵
-
-Released under the MIT license
-
-This software is provided 'as-is', without any express or implied warranty.
-In no event will the authors be held liable for any damages arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-   1. The origin of this software must not be misrepresented; you must not
-      claim that you wrote the original software. If you use this software
-      in a product, an acknowledgment in the product documentation would be
-      appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-      misrepresented as being the original software.
-
-   3. This notice may not be removed or altered from any source distribution. 
+ *  Zlib License
+ *  
+ *  Copyright (c) 2020 AoiKamishiro/神城葵
+ *  
+ *  
+ *  This software is provided 'as-is', without any express or implied warranty.
+ *  In no event will the authors be held liable for any damages arising from the use of this software.
+ *  
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *  
+ *     1. The origin of this software must not be misrepresented; you must not
+ *           claim that you wrote the original software. If you use this software
+ *           in a product, an acknowledgment in the product documentation would be
+ *           appreciated but is not required.
+ *           
+ *     2. Altered source versions must be plainly marked as such, and must not be
+ *          misrepresented as being the original software.
+ *          
+ *     3. This notice may not be removed or altered from any source distribution. 
+ *     
  */
 
 //LastUpdate:2020/07/24(JST) 
@@ -41,6 +43,8 @@ public class AKAssetsCompressor : EditorWindow
 
     //GUI Component
     private const string version = "AssetCompresser V1.2 by 神城葵";
+    private const string linktext = "操作説明等はこちら";
+    private const string link = "https://github.com/AoiKamishiro/UnityCustomEditor_AssetsCompressor";
     private int toolberSelection = 0;
 
     private Vector2 texture_ScrollPosition = Vector2.zero;
@@ -185,6 +189,7 @@ public class AKAssetsCompressor : EditorWindow
         using (new GUILayout.VerticalScope())
         {
             EditorGUILayout.LabelField(version);
+            if (GUILayout.Button(linktext)) { OpenLink(link); }
 
             EditorGUILayout.Space();
 
@@ -393,6 +398,10 @@ public class AKAssetsCompressor : EditorWindow
                 EditorGUILayout.Space();
             }
         }
+    }
+    private void OpenLink(string link)
+    {
+        Application.OpenURL(link);
     }
     private void BatchProcess_Texture()
     {
